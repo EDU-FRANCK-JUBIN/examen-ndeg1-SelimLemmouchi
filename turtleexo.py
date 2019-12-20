40a1768d011f4c595f6faba0694f19ca93bcd6af
Selim Lemmouchi <selim.lemmmouchi@hubcollab.com>
2019-12-20 11:56:52 +0100

    turtle

---
+++ turtleexo.py
@@ -0,0 +1,44 @@
+import turtle
+
+m = turtle.Turtle()
+m.color("orange")  
+m.pensize(5)
+m.shape("turtle") 
+
+m.penup()
+m.goto(0, 30)
+
+# l = turtle.Turtle()
+# l.color("deep sky blue")  
+# l.pensize(5)
+# l.shape("turtle") 
+
+# m.goto(0, 100)
+
+r = turtle.Turtle()
+r.color("red")  
+r.pensize(5)
+r.shape("turtle") 
+
+# m.goto(0, 15)
+
+# s = turtle.Turtle()
+# s.color("dark slate gray")  
+# s.pensize(5)
+# s.shape("turtle")
+
+r.penup()
+r.goto(0, 100)
+
+d = turtle.Turtle()
+d.color("purple")  
+d.pensize(5)
+d.shape("turtle") 
+
+
+
+input()
+
+pred1 = int(input('Please enter your prediction player 1: \n'))
+pred2 = int(input('Please enter your prediction player 2: \n'))
+
